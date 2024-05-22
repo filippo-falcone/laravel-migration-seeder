d03a9788af16691acf1a29e486dab1768c5606f2
filippo-falcone <falconefilippo98@hotmail.com>
2024-05-22 19:00:23 +0200

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Train;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $trains = Train::all();
+        $data = [
+            'trains' => $trains
+        ];
+        return view('home', $data);
+    }
+}
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i <= 10; $i++) {
+            $newtrain = new Train();
+            $newtrain->operator = $faker->randomElement(['trenord', 'trenitalia', 'italo', 'Trenitalia Tper', 'Ferrovie del Sud Est']);
+            $newtrain->departure_station = $faker->city();
+            $newtrain->arrival_station = $faker->city();
+            $newtrain->departure_time = $faker->time();
+            $newtrain->arrival_time = $faker->time();
+            $newtrain->train_code = $faker->numberBetween(1000, 9999);
+            $newtrain->wagons = $faker->numberBetween(1, 20);
+            $newtrain->on_time = $faker->boolean();
+            $newtrain->is_cancelled = $faker->boolean();
+            $newtrain->save();
+        }
+    }
+}
